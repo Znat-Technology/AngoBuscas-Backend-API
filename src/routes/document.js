2628b45eb243700f538b61144ddc2c6e6976b1ee
PatricioPedro <patriciowilderbarrospedro>
2021-05-12 14:45:22 +0100

    Testando rotas jna nuvem

--- src/routes/document.js
+++ src/routes/document.js
@@ -6,6 +6,10 @@ const DocumentController = require('../controllers/DocumentController')
  
 router.get('/', DocumentController.index)
 
+router.get('/mensagem', (req, res, next) => {
+  res.json({msg: "ok"})
+})
+
 router.get('/submit', DocumentController.submited)
 
 router.put('/search', DocumentController.search)
